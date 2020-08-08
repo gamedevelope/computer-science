6c442854b317134785d35df1e823ea39f760320e
liutengda <alisper@outlook.com>
2020-08-08 19:06:27 +0800

    练习1.36 使用 average daming 方法加速勋章不动点

--- SICP/1.3用高阶函数做抽象.rkt
+++ SICP/1.3用高阶函数做抽象.rkt
@@ -222,3 +222,93 @@
 (ff square)
 (ff (lambda (x) (* x (inc x))))
 ; (ff ff)
+
+; 章节 1.3.3
+; 使用 half-interval method 寻找方程的根
+(define (average a b)
+  (/ (+ a b) 2))
+
+(define (close-enough? a b)
+  (< (abs (- a b)) 0.00000001))
+
+(define (search f neg-point pos-point)
+  (let ((mid-point (average neg-point pos-point)))
+    (if (close-enough? neg-point pos-point)
+        mid-point
+        (let ((test-value (f mid-point)))
+          (cond ((positive? test-value)
+                 (search f neg-point mid-point))
+                ((negative? test-value)
+                 (search f mid-point pos-point))
+                (else mid-point))))))
+
+(search (lambda (x) (* x x)) -1 1)
+(search (lambda (x) (* x x x)) -10 1)
+
+; 增加容错处理
+(define (half-interval-method f a b)
+  (let ((a-value (f a))
+        (b-value (f b)))
+    (cond ((and (negative? a-value) (positive? b-value))
+           (search f a b))
+          ((and (negative? b-value) (positive? a-value))
+           (search f b a))
+          (else
+           (error "Values are not of opposite sign" a b)))))
+
+(half-interval-method sin 2.0 4.0)
+(half-interval-method (lambda (x) (- (* x x x) (* 2 x) 3))
+                      1.0
+                      2.0)
+
+; 寻找函数的不动点
+(display "寻找函数的不动点")
+(newline)
+
+(define tolerance 0.00001)
+(define (fixed-point f first-guess)
+  (define (close-enough? v1 v2)
+    (< (abs (- v1 v2)) tolerance))
+  (define (try guess)
+    (display guess)
+    (newline)
+    (let ((next (f guess)))
+      (if (close-enough? guess next)
+          next
+          (try next))))
+  (try first-guess))
+
+(fixed-point cos 1.0)
+(fixed-point (lambda (x) (+ (sin x) (cos x)))
+             1.0)
+
+(define (sqrt x)
+  (fixed-point (lambda (y) (average y (/ x y)))
+               1.0))
+
+(sqrt 100)
+
+; 练习 1.35
+; f(x) = 1 + 1/x 的不动点是黄金分割点
+(newline)
+(display "寻找 f(x) = 1 + 1/x 的不动点, 不使用 average damping 方法")
+(fixed-point (lambda (x) (+ 1 (/ 1 x)))
+             1.0)
+
+(newline)
+(display "寻找 f(x) = 1 + 1/x 的不动点, 使用 average damping 方法")
+(fixed-point (lambda (x) (average x (+ 1 (/ 1 x))))
+             1.0)
+
+; 练习 1.36
+; 打印寻找不动点过程中的一系列选值
+(newline)
+(display "求解 x^x = 1000 不使用 average damping 方法")
+(fixed-point (lambda (x) (/ (log 1000) (log x)))
+             2.0)
+
+(newline)
+(display "求解 x^x = 1000 使用 average damping 方法")
+(fixed-point (lambda (x) (average x (/ (log 1000) (log x))))
+             2.0)
+
