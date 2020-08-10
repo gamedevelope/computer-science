0cee75b5a86f8e6472a41f09eac85b888835112e
liutengda <alisper@outlook.com>
2020-08-10 23:58:28 +0800

    make-rat 可以处理正负数

--- SICP/Chapter2.rkt
+++ SICP/Chapter2.rkt
@@ -6,6 +6,17 @@
       m
       (gcd n (remainder m n))))
 
+; 章节分割
+(define (new-section msg)
+  (newline)
+  (newline)
+  (display "* * * * * * *")
+  (newline)
+  (display msg)
+  (newline)
+  (display "* * * * * * *")
+  (newline))
+
 ; 第二章
 ; 构建数据抽象
 
@@ -13,12 +24,12 @@
 ; 有理数的算术运算
 
 ; 构建一个有理数
-(define (make-rat n d)
-  (cond ((= d 0) (error "should not be zero"))
-        (else
-         (let ((c (gcd n d)))
-           (cons (/ n c)
-                 (/ d c))))))
+;(define (make-rat n d)
+;  (cond ((= d 0) (error "should not be zero"))
+;        (else
+;         (let ((c (gcd n d)))
+;           (cons (/ n c)
+;                 (/ d c))))))
 
 (define (numer rat)
   (car rat))
@@ -63,3 +74,18 @@
 (dev-rat x1 y1)
 (print-rat x1)
 (print-rat (dev-rat x1 y1))
+
+(new-section "练习 2.1")
+; 定义更好的 make-rat
+; 分子，分母都为负数，将其转换成正数
+; 如果分母为负，分子为整，将分子转换负数，分母转为正数
+(define (make-rat n d)
+  (cond ((= d 0) (error "should not be zero"))
+        (else
+         (let ((c (gcd n d))
+               (nd (* n d)))               
+           (cons (/ (/ nd d) c)
+                 (/ (/ nd  c))))))
+
+(make-rat 1 -1)
+(make-rat -1 -2)
