1bfac4f9c501f481849a7170843e35a76a12e60b
liutengda <alisper@outlook.com>
2020-08-10 13:30:17 +0800

    修正错误

--- SICP/Chapter2.rkt
+++ SICP/Chapter2.rkt
@@ -8,31 +8,47 @@
 
 ; 构建一个有理数
 (define (make-rat n d)
-  (list n d))
+  (cons n d))
 (define (numer rat)
   (car rat))
 (define (denom rat)
-  (cadr rat))
+  (cdr rat))
 
 ; 加法
 (define (add-rat x y)
-  (/ (+ (* (numer x) (denom y))
-        (* (numer y) (denom x)))
-     (* (denom x) (denom y))))
+  (make-rat (+ (* (numer x) (denom y))
+               (* (numer y) (denom x)))
+            (* (denom x) (denom y))))
 
 ; 减法
 (define (sub-rat x y)
-  (/ (- (* (numer x) (denom y))
-        (* (numer y) (denom x)))
-     (* (denom x) (denom y))))
+  (make-rat (- (* (numer x) (denom y))
+               (* (numer y) (denom x)))
+            (* (denom x) (denom y))))
 
 ; 乘法
 (define (mul-rat x y)
-  (/ (* (numer x) (numer y))
-     (* (denom x) (denom y))))
+  (make-rat (* (numer x) (numer y))
+            (* (denom x) (denom y))))
 
 ; 除法
 (define (dev-rat x y)
-  (/ (* (numer x) (denom y))
-     (* (denom x) (numer y))))
+  (make-rat (* (numer x) (denom y))
+            (* (denom x) (numer y))))
 
+; 打印
+(define (print-rat p)
+  (newline)
+  (display (numer p))
+  (display "/")
+  (display (denom p)))
+
+(define x1 (make-rat 5 3))
+(define y1 (make-rat 1 5))
+
+(add-rat x1 y1)
+(sub-rat x1 y1)
+(mul-rat x1 y1)
+(dev-rat x1 y1)
+(print-rat x1)
+(print-rat (dev-rat x1 y1))
