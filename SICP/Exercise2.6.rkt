d0955ba17031ee5215ab5d36edb4fe9ef8755ef4
liutengda <alisper@outlook.com>
2020-08-12 09:39:27 +0800

    写出正确的加法

--- SICP/Exercise2.6.rkt
+++ SICP/Exercise2.6.rkt
@@ -8,5 +8,19 @@
 (define two (lambda (f) (lambda (x) (f (f x)))))
 (define three (lambda (f) (lambda (x) (f (f (f x))))))
 
+; 定义加法
 (define (+ p1 p2)
+  (lambda (f) (lambda (x) ((p1 f) ((p2 f) x)))))
+
+; 定义乘法
+(define (* p1 p2)
   (lambda (f) (lambda (x) ((p1 (p2 f)) x))))
+
+(define n9 (* three three))
+((n9 inc) 1)
+
+(define n18 (+ n9 n9))
+((n18 inc) 1)
+
+(define n81 (* n9 n9))
+((n81 inc) 1)
