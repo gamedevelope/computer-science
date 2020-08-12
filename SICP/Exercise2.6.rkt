adccc0bb52a76d9a24ba89155f4c9404b5938bf2
liutengda <alisper@outlook.com>
2020-08-12 08:46:35 +0800

    练习 2.6 定义 one two 和 加号运算符

--- SICP/Exercise2.6.rkt
+++ SICP/Exercise2.6.rkt
@@ -3,3 +3,10 @@
 (define zero (lambda (f) (lambda (x) x)))
 (define (add-1 n)
   (lambda (f) (lambda (x) (f ((n f) x)))))
+
+(define one (lambda (f) (lambda (x) (f x))))
+(define two (lambda (f) (lambda (x) (f (f x)))))
+(define three (lambda (f) (lambda (x) (f (f (f x))))))
+
+(define (+ p1 p2)
+  (lambda (f) (lambda (x) ((p1 (p2 f)) x))))
