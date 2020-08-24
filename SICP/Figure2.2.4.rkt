41c13f59fd2eabe1fd9d7644e356f510feae53a7
liutengda <alisper@outlook.com>
2020-08-24 08:18:02 +0800

    练习 2.45

--- SICP/Figure2.2.4.rkt
+++ SICP/Figure2.2.4.rkt
@@ -45,3 +45,35 @@
           (beside (below painter top-left)
                   (below bottom-right corner))))))
 (paint (corner-split wave 3))
+
+; 练习 2.45
+(define (square-of-four tl tr bl br)
+  (lambda (painter)
+    (let ((top (beside (tl painter) (tr painter)))
+          (bottom (beside (bl painter) (br painter))))
+      (below bottom top))))
+
+(define (flipped-pairs-2.45 painter)
+  (let ((combine4 (square-of-four identity flip-vert
+                                  identity flip-vert)))
+    (combine4 painter)))
+
+(paint (flipped-pairs-2.45 wave))
+(paint ((square-of-four identity flip-horiz flip-vert rotate180) wave))
+
+(define (square-limit painter n)
+  (let ((combine4 (square-of-four flip-horiz identity
+                                  rotate180 flip-vert)))
+    (combine4 (corner-split painter n))))
+
+(paint (square-limit wave 3))
+(paint (right-split wave 2))
+
+(define (split tl tr)
+  (lambda (painter)
+    (tl painter (tr painter painter))))
+
+(define right-split-v2 (split beside below))
+(paint (right-split-v2 wave))
+(define up-split-v2 (split below beside))
+(paint (up-split-v2 wave))
