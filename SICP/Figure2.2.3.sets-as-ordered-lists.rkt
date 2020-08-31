68290370e7f50b4ef952aab6e87167f252058e30
liutengda <alisper@outlook.com>
2020-08-31 23:31:37 +0800

    练习 2.61 2.62 集合操作

--- SICP/Figure2.2.3.sets-as-ordered-lists.rkt
+++ SICP/Figure2.2.3.sets-as-ordered-lists.rkt
@@ -23,3 +23,26 @@
                (intersection-set s1 (cdr s2)))))))
 
 (intersection-set '(1 2 3 4) '(3 4 5 6))
+
+; 练习 2.61
+(define (adjoin-set x s)
+  (cond ((null? s) (cons x '()))
+        ((= x (car s)) s)
+        ((< x (car s)) (cons x s))
+        (else
+         (cons (car s)
+               (adjoin-set x (cdr s))))))
+
+; 练习 2.62
+(define (union-set s1 s2)
+  (cond ((null? s1) s2)
+        ((null? s2) s1)
+        (else
+         (let ((x1 (car s1))
+               (x2 (car s2)))
+           (cond ((= x1 x2)
+                  (cons x1 (union-set (cdr s1) (cdr s2))))
+                 ((< x1 x2)
+                  (cons x1 (union-set (cdr s1) s2)))
+                 (else
+                  (cons x2 (union-set s1 (cdr s2)))))))))
