22adb95079ba9ed9e3d0bbd14c48e41d0a812263
liutengda <alisper@outlook.com>
2020-09-22 23:16:42 +0800

    符号代数

---
+++ SICP/Figure2.5.3.rkt
@@ -0,0 +1,28 @@
+#lang sicp
+
+(#%require "Common.rkt")
+
+; 2.5.3 符号代数
+(define (install-polynomial-package)
+  (define (make-poly variable term-list)
+    (cons variable term-list))
+  (define (variable p) (car p))
+  (define (term-list p) (cdr p))
+  (define (variable? x) (symbol? x))
+  (define (same-variable? v1 v2)
+    (and (variable? v1) (variable? v2) (eq? v1 v2)))
+  (define (add-poly p1 p2)
+  (if (same-variable? (variable p1) (variable p2))
+      (make-poly (variable p1)
+                 (add-terms (term-list p1)
+                            (term-list p2)))
+      (error "Polys not in same var -- ADD-POLY"
+             (list p1 p2))))
+  (define (mul-poly p1 p2)
+  (if (same-variable? (variable p1) (variable p2))
+      (make-poly (variable p1)
+                 (mul-terms (term-list p1)
+                            (term-list p2)))
+      (error "Polys not in same var -- MUL-POLY"
+             (list p1 p2))))
+  'done)
