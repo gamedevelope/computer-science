2a95b221f07f93439b3d75ddcbe5245f1dcddc77
liutengda <alisper@outlook.com>
2020-09-25 14:31:24 +0800

    练习 3.14

--- SICP/Figure3.3.1.rkt
+++ SICP/Figure3.3.1.rkt
@@ -15,3 +15,16 @@
 (define (make-cycle x)
   (set-cdr! (last-pair x) x)
   x)
+
+; 练习 3.14
+(define (mystery x)
+  (define (loop x y)
+    (if (null? x)
+        y
+        (let ((temp (cdr x)))
+          (set-cdr! x y)
+          (loop temp x))))
+  (loop x '()))
+
+(let ((x (list 'a 'b 'c 'd 'e)))
+  (mystery x))
