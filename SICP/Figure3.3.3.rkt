c481ae43b902c89ce75bfe44533f8955d5b5a83c
liutengda <alisper@outlook.com>
2020-10-04 21:52:28 +0800

    Figure 3.3.3 创建局部表格

--- SICP/Figure3.3.3.rkt
+++ SICP/Figure3.3.3.rkt
@@ -12,24 +12,48 @@
         ((equal? key (caar records)) (car records))
         (else (assoc key (cdr records)))))
 
-(define (insert! key value table)
-  (let ((record (assoc key (cdr table))))
-    (if record
-        (set-cdr! record value)
-        (set-cdr! table
-                  (cons (cons key value) (cdr table)))))
-  'ok)
+;(define (insert! key value table)
+;  (let ((record (assoc key (cdr table))))
+;    (if record
+;        (set-cdr! record value)
+;        (set-cdr! table
+;                  (cons (cons key value) (cdr table)))))
+;  'ok)
 
 (define (make-table)
-  (list '*table*))
+  (let ((local-table (list '*table*)))
+    (define (lookup key-1 key-2)
+      (let ((subtable
+             (assoc key-1 (cdr local-table))))
+        (if subtable
+            (let ((record
+                   (assoc key-2 (cdr subtable))))
+              (if record
+                  (cdr record)
+                  false))
+            false)))
 
-(define (lookup key-1 key-2 table)
-  (let ((subtable
-         (assoc key-1 (cdr table))))
-    (if subtable
-        (let ((record
-               (assoc key-2 (cdr subtable))))
-          (if record
-              (cdr record)
-              false))
-        false)))
+    (define (insert! key-1 key-2 value)
+      (let ((subtable (assoc key-1 (cdr local-table))))
+        (if subtable
+            (let ((record (assoc key-2 (cdr subtable))))
+              (if record
+                  (set-cdr! record value)
+                  (set-cdr! subtable
+                            (cons (cons key-2 value)
+                                  (cdr subtable)))))
+            (set-cdr! local-table
+                      (cons (list key-1
+                                  (cons key-2 value))
+                            (cdr local-table))))))
+    (define (dispatch m)
+      (cond ((eq? m 'lookup-proc) lookup)
+            ((eq? m 'insert-proc!) insert!)
+            ((eq? m 'print) (display local-table))
+            (else (error "Unknown operation -- TABLE" m))))
+    dispatch))
+
+(let ((t (make-table)))
+  ((t 'insert-proc!) 'letters 'a 97)
+  ((t 'insert-proc!) 'letters 'b 98)
+  (t 'print))
