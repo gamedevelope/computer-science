142d914a3a142fcbf6ff0f480deebb250a7b0b84
liutengda <alisper@outlook.com>
2020-10-06 18:41:22 +0800

    练习 3.25

--- SICP/Figure3.3.3.rkt
+++ SICP/Figure3.3.3.rkt
@@ -77,4 +77,55 @@
   (display ((t 'lookup-proc) 'letters 0.0000000001))
   (display ((t 'lookup-proc) 'letters 'a)))
 
+; 练习 3.25
+; n 维表格
+;(define (make-table-v3.25)
+;  (let ((local-table (list '*table*)))
+;    (define (lookup key-1 key-2)
+;      (let ((subtable
+;             (assoc key-1 (cdr local-table))))
+;        (if subtable
+;            (let ((record
+;                   (assoc key-2 (cdr subtable))))
+;              (if record
+;                  (cdr record)
+;                  false))
+;            false)))
+;
+;    (define (insert! key . value)
+;      (let ((table local-table))
+;        (define (loop)
+;          (
+;            
+;      (let ((subtable (assoc key-1 (cdr local-table))))
+;        (if subtable
+;            (let ((record (assoc key-2 (cdr subtable))))
+;              (if record
+;                  (set-cdr! record value)
+;                  (set-cdr! subtable
+;                            (cons (cons key-2 value)
+;                                  (cdr subtable)))))
+;            (set-cdr! local-table
+;                      (cons (list key-1
+;                                  (cons key-2 value))
+;                            (cdr local-table))))))
+;    (define (dispatch m)
+;      (cond ((eq? m 'lookup-proc) lookup)
+;            ((eq? m 'insert-proc!) insert!)
+;            ((eq? m 'print) (display local-table))
+;            (else (error "Unknown operation -- TABLE" m))))
+;    dispatch))
 
+(define (f x . y)
+  (display x)
+  (if (> (length y) 1)
+      (f (car y) (cdr y))))
+
+;(define (f x . y)
+;  (define (iter a b)
+;    (if (null? a)
+;        (display a)
+;        (begin
+;          (display a)
+;          (iter (car b) (cdr b)))))
+;  (iter x y))
