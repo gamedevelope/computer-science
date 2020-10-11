29d31765541425affd3ab1726129bb6fc15fa889
liutengda <alisper@outlook.com>
2020-10-11 14:32:16 +0800

    练习 3.25 完结

--- SICP/Figure3.3.3.rkt
+++ SICP/Figure3.3.3.rkt
@@ -1,24 +1,24 @@
 #lang sicp
 
 ; 表格的表示
-;(define (lookup key table)
-;  (let ((record (assoc key (cdr table))))
-;        (if record
-;            (cdr record)
-;            false)))
+(define (lookup key table)
+  (let ((record (assoc key (cdr table))))
+    (if record
+        (cdr record)
+        false)))
 
-;(define (assoc key records)
-;  (cond ((null? records) false)
-;        ((equal? key (caar records)) (car records))
-;        (else (assoc key (cdr records)))))
+(define (assoc key records)
+  (cond ((null? records) false)
+        ((equal? key (caar records)) (car records))
+        (else (assoc key (cdr records)))))
 
-;(define (insert! key value table)
-;  (let ((record (assoc key (cdr table))))
-;    (if record
-;        (set-cdr! record value)
-;        (set-cdr! table
-;                  (cons (cons key value) (cdr table)))))
-;  'ok)
+(define (insert! key value table)
+  (let ((record (assoc key (cdr table))))
+    (if record
+        (set-cdr! record value)
+        (set-cdr! table
+                  (cons (cons key value) (cdr table)))))
+  'ok)
 
 ; 练习 3.24
 (define (same-key? key-1 key-2)
@@ -30,11 +30,6 @@
              (let ((d (- key-1 key-2)))
                (and (< d 0.000001)
                     (> d -0.000001)))))))
-
-(define (assoc key records)
-  (cond ((null? records) false)
-        ((same-key? key (caar records)) (car records))
-        (else (assoc key (cdr records)))))
 
 (define (make-table)
   (let ((local-table (list '*table*)))
@@ -79,55 +74,31 @@
 
 ; 练习 3.25
 ; n 维表格
-(define (make-table-v3.25)
+(define (make-table-3.25)
   (let ((local-table (list '*table*)))
-    (define (lookup key-1 key-2)
-      (let ((subtable
-             (assoc key-1 (cdr local-table))))
-        (if subtable
-            (let ((record
-                   (assoc key-2 (cdr subtable))))
-              (if record
-                  (cdr record)
-                  false))
+    (define (lookup key)
+      (let ((record (assoc key (cdr local-table))))
+        (if record
+            (cdr record)
             false)))
-
-    (define (insert! key . value)
-      (define (insert table key value)
-        (let ((subtable (assoc key (cdr table))))
-          (if subtable
-              (if (null? (cdr value))
-                  (set-cdr! subtable
-                            (cons (cons key (car value))
-                                  (cdr subtable)))
-                  (insert subtable (car value) (cdr value)))
-              (set-cdr! table
-                        (cons (insert )
-                              (cdr table)))))))
-      (insert local-table key value))
-                    
-            
-      (let ((subtable (assoc key-1 (cdr local-table))))
-        (if subtable
-            (let ((record (assoc key-2 (cdr subtable))))
-              (if record
-                  (set-cdr! record value)
-                  (set-cdr! subtable
-                            (cons (cons key-2 value)
-                                  (cdr subtable)))))
+    
+    (define (insert! key value)
+      (let ((record (assoc key (cdr local-table))))
+        (if record
+            (set-cdr! record value)
             (set-cdr! local-table
-                      (cons (list key-1
-                                  (cons key-2 value))
-                            (cdr local-table))))))
+                      (cons (cons key value) (cdr local-table))))))
+
     (define (dispatch m)
-      (cond ((eq? m 'lookup-proc) lookup)
-            ((eq? m 'insert-proc!) insert!)
-            ((eq? m 'print) (display local-table))
-            (else (error "Unknown operation -- TABLE" m))))
+      (cond ((eq? m 'lookup) lookup)
+            ((eq? m 'insert!) insert!)
+            ((eq? m 'print) (display local-table))))
     dispatch))
-
-;(define (f x . y)
-;  (display x)
-;  (if (> (length y) 1)
-;      (f (car y) (cdr y))))
-
+(newline)
+(let ((t (make-table-3.25)))
+  ((t 'insert!) (list 'a 'b 'c) 100)
+  ((t 'insert!) (list 'a 'b) 100)
+  ((t 'insert!) (list 'a 'c) 100)
+  ((t 'insert!) (list 'b 'c) 100)
+  (display ((t 'lookup) (list 'b 'c)))
+  (t 'print))
