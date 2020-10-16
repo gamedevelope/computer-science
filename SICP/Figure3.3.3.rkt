1c9afb3da07bbcca8aa0cb81c0d68b9c4d640d3f
liutengda <alisper@outlook.com>
2020-10-16 23:41:54 +0800

    练习 3.27

--- SICP/Figure3.3.3.rkt
+++ SICP/Figure3.3.3.rkt
@@ -228,3 +228,30 @@
   (display ((t 'lookup) (list 'b)))
   (display ((t 'lookup) (list 'b 'c)))
   (display ((t 'lookup) (list 'b 'd))))
+
+; 练习 3.27
+(define (fib n)
+  (cond ((= n 0) 0)
+        ((= n 1) 1)
+        (else
+         (+ (fib (- n 1))
+            (fib (- n 2))))))
+
+(define (make-table-3.27)
+  (list '*table))
+
+(define (memoize f)
+  (let ((table (make-table-3.27)))
+    (lambda (x)
+      (let ((priviously-computed-result (lookup x table)))
+        (or priviously-computed-result
+            (let ((result (f x)))
+              (insert! x result table)
+              result))))))
+
+(define memo-fib
+  (memoize (lambda (n)
+             (cond ((= n 0) 0)
+                   ((= n 1) 1)
+                   (else (+ (memo-fib (- n 1))
+                            (memo-fib (- n 2))))))))
