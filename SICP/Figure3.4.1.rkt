273ec0d5fcbfb538bc82b55137ad1c65cc290ae7
liutengda <alisper@outlook.com>
2020-11-02 23:43:32 +0800

    原版code

--- SICP/Figure3.4.1.rkt
+++ SICP/Figure3.4.1.rkt
@@ -22,3 +22,42 @@
           (mutex 'release)
           val))
       serialized-p)))
+
+(define disallow-preempt-current-thread
+  (access disallow-preempt-current-thread
+          (->environment '(runtime thread))))
+
+(define allow-preempt-current-thread
+  (access allow-preempt-current-thread
+          (->environment '(runtime thread))))
+
+(define (kill-thread thread)
+  (let ((event
+         (lambda ()
+           (exit-current-thread 'RIP))))
+    (without-interrupts
+     (lambda ()
+       (case (thread-execution-state thread)
+         ((STOPPED) (restart-thread thread #t event))
+         ((DEAD) unspecific)
+         (else (signal-thread-event thread event)))))))
+
+
+(define (parallel-execute . thunks)
+  (let ((my-threads '()))
+    (define (terminator)
+      (without-interrupts
+       (lambda ()
+         (for-each kill-thread my-threads)
+         (set! my-threads '())
+         unspecific)))
+    (without-interrupts
+     (lambda ()
+       (set! my-threads
+             (map (lambda (thunk)
+                    (let ((thread (create-thread #f thunk)))
+                      (detach-thread thread)
+                      thread))
+                  thunks))
+       unspecific))
+    terminator))
