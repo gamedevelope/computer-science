8f3012c5d543cd47f214e6534831b2342af8dda7
liutengda <alisper@outlook.com>
2020-11-06 12:03:15 +0800

    练习 3.42

--- SICP/Figure3.4.1.rkt
+++ SICP/Figure3.4.1.rkt
@@ -81,3 +81,65 @@
 ((a 'withdraw) 10)
 ((a 'withdraw) 10)
 (a 'balance)
+
+; 练习 3.41
+(define (make-account balance)
+  (define (withdraw amount)
+    (if (>= balance amount)
+        (begin (set! balance (- balance amount))
+               balance)
+        "Insufficient funds"))
+  (define (deposit amount)
+    (set! balance (+ balance amount))
+    balance)
+  (let ((protected (make-serializer)))
+    (define (dispatch m)
+      (cond ((eq? m 'withdraw) (protected withdraw))
+            ((eq? m 'deposit) (protected deposit))
+            ((eq? m 'balance) ((protected (lambda () balance))))
+            (else (error "Unknown request -- MAKE-ACCOUNT"
+                         m))))
+    dispatch))
+
+(define a (make-account 100))
+(a 'balance)
+((a 'withdraw) 10)
+(a 'balance)
+
+;(set! (a 'balance) 1000)
+;(a 'balance)
+
+; 练习 3.42
+(define (make-account balance)
+  (define (withdraw amount)
+    (if (>= balance amount)
+        (begin (set! balance (- balance amount))
+               balance)
+        "Insufficient funds"))
+  (define (deposit amount)
+    (set! balance (+ balance amount))
+    balance)
+  (let ((protected (make-serializer)))
+    (let ((protected-withdraw (protected withdraw))
+          (protected-deposit (protected deposit)))
+      (define (dispatch m)
+        (cond ((eq? m 'withdraw) protected-withdraw)
+              ((eq? m 'deposit) protected-deposit)
+              ((eq? m 'balance) balance)
+              (else (error "Unknown request -- MAKE-ACCOUNT"
+                           m))))
+      dispatch)))
+
+(define a (make-account 100))
+((a 'withdraw) 10)
+(a 'balance)
+
+((a 'withdraw) 10)
+(a 'balance)
+
+(parallel-execute
+ (lambda () ((a 'withdraw) 10))
+ (lambda () ((a 'withdraw) 10))
+ (lambda () ((a 'withdraw) 10)))
+
+(a 'balance)
