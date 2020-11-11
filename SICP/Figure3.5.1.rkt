e12b6b42e72b9eb9f0f8a1e298cba1536ac4237f
liutengda <alisper@outlook.com>
2020-11-11 13:30:14 +0800

    章节 3.5.1

--- SICP/Figure3.5.1.rkt
+++ SICP/Figure3.5.1.rkt
@@ -4,3 +4,41 @@
 
 (stream-car cs)
 (stream-cdr cs)
+
+(define (stream-ref s n)
+  (if (= n 0)
+      (stream-car s)
+      (stream-ref (stream-cdr s) (- n 1))))
+
+(define (stream-map proc s)
+  (if (stream-null? s)
+      the-empty-stream
+      (cons-stream (proc (stream-car s))
+                   (stream-map proc (stream-cdr s)))))
+
+(define (stream-for-each proc s)
+  (if (stream-null? s)
+      'done
+      (begin (proc (stream-car s)
+                   (stream-for-each proc (stream-cdr s))))))
+
+(define (display-stream s)
+  (stream-for-each display-line s))
+
+(define (display-line x)
+  (newline)
+  (display x))
+
+(define (stream-enumerate-interval low high)
+  (if (> low high)
+      the-empty-stream
+      (cons-stream
+       low
+       (stream-enumerate-interval (+ low 1) high))))
+
+(define stream1 (stream-enumerate-interval 1 10))
+(stream-car stream1)
+(stream-car (stream-cdr stream1))
+(stream-car (stream-cdr (stream-cdr stream1)))
+
+(display-stream stream1)
