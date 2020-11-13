ddf54833c0b7cd1d836857288fd9972ba32ddfd5
liutengda <alisper@outlook.com>
2020-11-13 20:11:27 +0800

    练习 3.5.1

--- SICP/Figure3.5.1.rkt
+++ SICP/Figure3.5.1.rkt
@@ -66,3 +66,13 @@
   (loop))
 
 (fib-end 1000)
+
+; 练习 3.51
+(define (show x)
+  (display-line x)
+  x)
+
+(define x (stream-map show (stream-enumerate-interval 0 10)))
+(stream-ref x 5)
+(stream-ref x 7)
+(stream-ref x 10)
