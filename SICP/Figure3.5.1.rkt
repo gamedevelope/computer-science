076fe24944c527331b30a277b9f27d2a9e82cd6d
liutengda <alisper@outlook.com>
2020-12-05 11:06:57 +0800

    练习 3.70

--- SICP/Figure3.5.1.rkt
+++ SICP/Figure3.5.1.rkt
@@ -415,8 +415,41 @@
 (stream-values (check (pairs integers integers) integers) 5)
 
 ; 练习 3.70
-(define (weight))
+(define (merge s1 s2)
+  (cond ((stream-null? s1) s2)
+        ((stream-null? s2) s1)
+        (else
+         (let ((s1car (stream-car s1))
+               (s2car (stream-car s2)))
+           (cond ((< s1car s2car)
+                  (cons-stream s1car
+                               (merge (stream-cdr s1) s2)))
+                 ((> s1car s2car)
+                  (cons-stream s2car
+                               (merge s1 (stream-cdr s2))))
+                 (else
+                  (cons-stream s1car
+                               (merge (stream-cdr s1)
+                                      (stream-cdr s2)))))))))
+(define (weight a b)
+  (+ a b))
 
-(define (merge-weighted s t)
-  )
+(define (merge-weighted s1 s2 weight)
+  (cond ((stream-null? s1) s2)
+        ((stream-numm? s2) s1)
+        (else
+         (let ((s1v1 (stream-car s1))
+               (s1v2 (stream-car (stream-cdr s1)))
+               (s2v1 (stream-car s2))
+               (s2v2 (stream-car (stream-cdr s2))))
+           (cond ((< (weight s1v1 s1v2) (weight s2v1 s2v2))
+                  (cons-stream s1v1
+                               (merge-weighted (stream-cdr s1) s2)))
+                 ((< (weight s2v1 s2v2) (weight s1v1 s1v2))
+                  (cons-stream s2v1
+                               (merge-weighted s1 (stream-cdr s2))))
+                 (else
+                  (cons-stream s1v1
+                               (merge-weighted (stream-cdr s1)
+                                               (stream-cdr s2)))))))))
 
