028ef374db213e53d1a5d0e7363ad7b479fc643f
liutengda <alisper@outlook.com>
2021-01-30 20:21:40 +0800

    do的语法

--- SICP/Figure4.interp.rkt
+++ SICP/Figure4.interp.rkt
@@ -323,6 +323,18 @@
   (put 'eval 'let* eval-let*))
 (install-let*->nested-lets)
 
+;;; 定义 do
+;;; do 的语法
+;;;(do ((vec (make-vector 5))
+;;;     (i 0 (+ i 1)))
+;;;    ((= i 5) vec)
+;;;  (vector-set! vec i i))    =>  #(0 1 2 3 4)
+;;;
+;;;(let ((x '(1 3 5 7 9)))
+;;;  (do ((x x (cdr x))
+;;;       (sum 0 (+ sum (car x))))
+;;;      ((null? x) sum)))     =>  25
+
 (define (setup-environment)
   (define primitive-procedures
     (list (list 'car car)
