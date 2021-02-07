53514cfb197a7a6d888233d8db7cfdcf6e8ef140
liutengda <alisper@outlook.com>
2021-02-07 21:07:56 +0800

    练习 4.21

---
+++ SICP/Figure.Interp.rkt
@@ -0,0 +1,10 @@
+#lang sicp
+(#%require "Figure.Interp.V2.rkt")
+(eval '((lambda (n)
+          ((lambda (fact)
+             (fact fact n))
+           (lambda (ft k)
+             (if (= k 1)
+                 1
+                 (* k (ft ft (- k 1)))))))
+        10) genv)
