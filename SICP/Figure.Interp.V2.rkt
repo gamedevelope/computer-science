0c0cdef68ce2ee9565197425bf833c008438b5f9
liutengda <alisper@outlook.com>
2021-03-02 23:48:37 +0800

    练习 4.21

--- SICP/Figure.Interp.V2.rkt
+++ SICP/Figure.Interp.V2.rkt
@@ -632,22 +632,44 @@
 (eval '(letrec () 100) genv)
 
 ;; 练习 4.21
-((lambda (n)
-   ((lambda (fact)
-      (fact fact n))
-    (lambda (ft k)
-      (if (= k 1)
-          1
-          (* k (ft ft (- k 1)))))))
- 10)
+(eval '((lambda (n)
+          ((lambda (fact)
+             (fact fact n))
+           (lambda (ft k)
+             (if (= k 1)
+                 1
+                 (* k (ft ft (- k 1)))))))
+        10) genv)
 
 ;; 练习 4.21a
-((lambda (n)
-   ((lambda (fib)
-      (fib fib n))
-    (lambda (fib k)
-      (if (<= k 2)
-          1
-          (+ (fib fib (- k 2)) (fib fib (- k 1)))))))
- 6)
-    
+(eval '((lambda (n)
+          ((lambda (fib)
+             (fib fib n))
+           (lambda (fib k)
+             (if (<= k 2)
+                 1
+                 (+ (fib fib (- k 2)) (fib fib (- k 1)))))))
+        6) genv)
+;; 练习 4.21b
+(eval '(define (f x)
+         (define (even? n)
+           (if (= n 0)
+               true
+               (odd? (- n 1))))
+         (define (odd? n)
+           (if (= n 0)
+               false
+               (even? (- n 1))))
+         (even? x)) genv)
+
+(eval '(define (f x)
+         ((lambda (even? odd?)
+            (even? even? odd? x))
+          (lambda (ev? od? n)
+            (if (= n 0)
+                true
+                (od? od? ev? (- n 1))))
+          (lambda (od? ev? n)
+            (if (= n 0)
+                false
+                (ev? ev? od? (- n 1)))))) genv)
