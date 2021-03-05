176998a1d06c052b9360f2fd5af037ab2cee8906
liutengda <alisper@outlook.com>
2021-03-05 21:33:35 +0800

    analyze-sequenceexps

--- SICP/Figure.Analyze.Interp.rkt
+++ SICP/Figure.Analyze.Interp.rkt
@@ -56,3 +56,16 @@
         (bproc (analyze-sequence (lambda-body exp))))
     (lambda (env) (make-procedure vars bproc env))))
 
+(define (analyze-sequenceexps)
+  (define (sequentially proc1 proc2)
+    (lambda (env) (proc1 env) (proc2 env)))
+
+  (define (loop first-proc rest-procs)
+    (if (null? rest-procs)
+        first-proc
+        (loop (sequentially first-proc (car rest-procs))
+              (cdr rest-procs))))
+  (let ((procs (map analyze exps)))
+    (if (null? procs)
+        (error "Empty sequence -- ANALYZE"))
+    (loop (car procs) (cdr procs))))
