5a84e78d63add8c36f9e414284da2f2542fd60da
liutengda <alisper@outlook.com>
2021-04-02 21:47:21 +0800

    list

--- SICP/Figure4.2.3.rkt
+++ SICP/Figure4.2.3.rkt
@@ -9,3 +9,27 @@
 (define x (cons 1 2))
 (car x)
 (cdr x)
+
+(define (list-ref items n)
+  (if (= n 0)
+      (car items)
+      (list-ref (cdr items) (- n 1))))
+
+(define (map proc items)
+  (if (null? items)
+      '()
+      (cons (proc (car items))
+            (map proc (cdr items)))))
+
+(define (scale-list items factor)
+  (map (lambda (x) (* x factor))
+       items))
+
+(define (add-lists list1 list2)
+  (cond ((null? list1) list2)
+        ((null? list2) list1)
+        (else (cons (+ (car list1) (car list2))
+                    (add-lists (cdr list1) (cdr list2))))))
+
+(define ones (cons 1 ones))
+(define (integers (cons 1 (add-lists ones integers))))
