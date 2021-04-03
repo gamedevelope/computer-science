2e2c4ca207e3bd982dc02a4eb0e8a6a7eb639932
liutengda <alisper@outlook.com>
2021-04-03 17:33:24 +0800

    自定义 流操作

--- SICP/Figure4.2.2.rkt
+++ SICP/Figure4.2.2.rkt
@@ -402,3 +402,29 @@
 (eval '(define x (cons 1 2)) genv)
 (eval '(car x) genv)
 (eval '(cdr x) genv)
+
+(eval '(define (list-ref items n)
+         (if (= n 0)
+             (car items)
+             (list-ref (cdr items) (- n 1)))) genv)
+
+(eval '(define (map proc items)
+         (if (null? items)
+             '()
+             (cons (proc (car items))
+                   (map proc (cdr items))))) genv)
+
+(eval '(define (scale-list items factor)
+         (map (lambda (x) (* x factor))
+              items)) genv)
+
+(eval '(define (add-lists list1 list2)
+         (cond ((null? list1) list2)
+               ((null? list2) list1)
+               (else (cons (+ (car list1) (car list2))
+                           (add-lists (cdr list1) (cdr list2)))))) genv)
+
+(eval '(define ones (cons 1 ones)) genv)
+(eval '(define (integers (cons 1 (add-lists ones integers)))) genv)
+
+(force-it (eval '(list-ref integers 10) genv))
