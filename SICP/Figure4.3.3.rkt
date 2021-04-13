69134661829f6edd639c15601b301e62ad553c18
liutengda <alisper@outlook.com>
2021-04-13 23:47:41 +0800

    移除注释的代码

--- SICP/Figure4.3.3.rkt
+++ SICP/Figure4.3.3.rkt
@@ -125,20 +125,6 @@
 (define (frame-variables frame) (car frame))
 (define (frame-values frame) (cdr frame))
 
-;(define (analyze-assignment exp)
-;  (let ((var (assignment-variable exp))
-;        (vproc (analyze (assignment-value exp))))
-;    (lambda (env)
-;      (set-variable-value! var (vproc env) env)
-;      'ok)))
-
-;(define (analyze-definition exp)
-;  (let ((var (definition-variable exp))
-;        (vproc (analyze (definition-value exp))))
-;    (lambda (env)
-;      (define-variable! var (vproc env) env)
-;      'ok)))
-
 (define (assignment-variable exp) (cadr exp))
 (define (assignment-value exp) (caddr exp))
 (define (if-predicate exp) (cadr exp))
@@ -149,15 +135,6 @@
         'false))
 
 (define (true? x) (not (eq? x false)))
-
-;(define (analyze-if exp)
-;  (let ((pproc (analyze (if-predicate exp)))
-;        (cproc (analyze (if-consequent exp)))
-;        (aproc (analyze (if-alternative exp))))
-;    (lambda (env)
-;      (if (true? (pproc env))
-;          (cproc env)
-;          (aproc env)))))
 
 ;;;SECTION 4.1.2
 (define (amb? exp) (tagged-list? exp 'amb))
@@ -360,42 +337,11 @@
   (list 'procedure parameters body env))
 
 (define (lambda-parameters exp) (cadr exp))
-  (define (lambda-body exp) (cddr exp))
-  (define (eval-lambda exp env)
-    (make-procedure (lambda-parameters exp)
-                    (lambda-body exp)
-                    env))
-
-;(define (analyze-lambda exp)
-;  (let ((vars (lambda-parameters exp))
-;        (bproc (analyze-sequence (lambda-body exp))))
-;    (lambda (env) (make-procedure vars bproc env))))
-
-;(define (analyze-sequence exps)
-;  (define (sequentially proc1 proc2)
-;    (lambda (env) (proc1 env) (proc2 env)))
-;
-;  (define (loop first-proc rest-procs)
-;    (if (null? rest-procs)
-;        first-proc
-;        (loop (sequentially first-proc (car rest-procs))
-;              (cdr rest-procs))))
-;  (let ((procs (map analyze exps)))
-;    (if (null? procs)
-;        (error "Empty sequence -- ANALYZE"))
-;    (loop (car procs) (cdr procs))))
-
-;;; 练习 4.23
-;(define (analyze-sequence exps)
-;  (define (execute-sequence procs env)
-;    (cond ((null? (cdr procs)) ((car procs) env))
-;          (else
-;           ((car procs) env)
-;           (execute-sequence (cdr procs) env))))
-;  (let ((procs (map analyze exps)))
-;    (if (null? procs)
-;        (error "Empty sequence -- ANALYZE"))
-;    (lambda (env) (execute-sequence procs env))))
+(define (lambda-body exp) (cddr exp))
+(define (eval-lambda exp env)
+  (make-procedure (lambda-parameters exp)
+                  (lambda-body exp)
+                  env))
 
 (define (expand-clauses clauses)
     (define (make-if predicate consequent alternative)
@@ -429,32 +375,6 @@
 (define (cond->if exp) (expand-clauses (cond-clauses exp)))
 (define (operator exp) (car exp))
 (define (operands exp) (cdr exp))
-;(define (analyze-application exp)
-;  (let ((fproc (analyze (operator exp)))
-;        (aprocs (map analyze (operands exp))))
-;    (lambda (env)
-;      (execute-application (fproc env)
-;                           (map (lambda (aproc) (aproc env))
-;                                aprocs)))))
-;
-;(define primitive-procedures
-;    (list (list 'car car)
-;          (list 'cdr cdr)
-;          (list 'cons cons)
-;          (list 'null? null?)
-;          (list '+ +)
-;          (list '- -)
-;          (list '* *)
-;          (list '/ /)
-;          (list '= =)
-;          (list '< <)
-;          (list '<= <=)
-;          (list '> >)
-;          (list '>= >=)
-;          (list 'make-vector make-vector)
-;          (list 'vector-set! vector-set!)
-;          (list 'display display)
-;          (list 'list list)))
 
 (define (tagged-list? exp tag)
   (if (pair? exp)
@@ -480,18 +400,6 @@
 (define (procedure-parameters p) (cadr p))
 (define (procedure-environment p) (cadddr p))
 (define (procedure-body p) (caddr p))
-;(define (execute-application proc args)
-;  (cond ((primitive-procedure? proc)
-;         (apply-primitive-procedure proc args))
-;        ((compound-procedure? proc)
-;         ((procedure-body proc)
-;          (extend-environment (procedure-parameters proc)
-;                              args
-;                              (procedure-environment proc))))
-;        (else
-;         (error
-;          "Unknown procedure type -- EXECUTE-APPLICATION"
-;          proc))))
 
 ;;; 
 (define (setup-environment)
