9e485bc64ecbff559d540e6b3aed8a9ebf302f88
liutengda <alisper@outlook.com>
2021-05-17 20:10:42 +0800

    parse juzi

--- SICP/Figure4.3.3.nlp.rkt
+++ SICP/Figure4.3.3.nlp.rkt
@@ -22,7 +22,7 @@
 (analyze '(define (parse-sentence)
             (list 'sentence
                   (parse-noun-phrase)
-                  (parse-word verbs))))
+                  (parse-verb-phrase))))
 
 (analyze '(define (parse-noun-phrase)
             (list 'noun-phrase
@@ -48,4 +48,28 @@
               (require (null? *unparsed*))
               sent)))
 ;(analyze '(parse '(the cat eats)))
+(analyze '(define prepositions '(prep for to in by with)))
+(analyze '(define (parse-prepositional-phrase)
+            (list 'prep-phrase
+                  (parse-word prepositions)
+                  (parse-noun-phrase))))
+(analyze '(define (parse-verb-phrase)
+            (define (maybe-extend verb-phrase)
+              (amb verb-phrase
+                   (maybe-extend (list 'verb-phrase
+                                       verb-phrase
+                                       (parse-prepositional-phrase)))))
+            (maybe-extend (parse-word verbs))))
+(analyze '(define (parse-simple-noun-phrase)
+            (list 'simple-noun-phrase
+                  (parse-word articles)
+                  (parse-word nouns))))
+(analyze '(define (parse-noun-phrase)
+            (define (maybe-extend noun-phrase)
+              (amb noun-phrase
+                   (maybe-extend (list 'noun-phrase
+                                       noun-phrase
+                                       (parse-prepositional-phrase)))))
+            (maybe-extend (parse-simple-noun-phrase))))
+
 (driver-loop)
