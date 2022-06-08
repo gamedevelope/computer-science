0838ed2451d741a00ac18c03747ec2968c11c619
orange <liutengda>
2022-06-08 19:49:15 +0800

    new function

--- SICP/common.rkt
+++ SICP/common.rkt
@@ -1,4 +1,16 @@
 #lang racket
 
+(#%provide abs
+           println
+           square
+           )
+
+(define (abs x)
+  (if (< x 0) (- x) x))
+
 (define (square x)
   (* x x))
+
+(define (println p)
+  (display p)
+  (newline))
