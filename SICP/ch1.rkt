daefc4e6e22ff66150ca6e2951b517607ea00a8a
orange <liutengda>
2022-06-09 21:42:53 +0800

    ex1.10

--- SICP/ch1.rkt
+++ SICP/ch1.rkt
@@ -14,21 +14,35 @@
 
 ;;; Exercise 1.8
 (define (ex1.8)
-  (define (cube-root-iter guess x)
-    (if (good-enough? guess x)
-        guess
-        (cube-root-iter (improve guess x) x)))
-  
-  (define (good-enough? guess x)
-    (< (abs (- (cube guess) x)) 0.001))
-  
-  (define (improve y x)
-    (/ (+ (/ x (square y))
-          (* 2 y))
-       3.0))
-  
   (define (cube-root x)
-    (cube-root-iter 1 x))
+    (define (cube-root-iter guess)
+      (if (good-enough? guess)
+          guess
+          (cube-root-iter (improve guess))))
+  
+    (define (good-enough? guess)
+      (< (abs (- (cube guess) x)) 0.001))
+  
+    (define (improve y)
+      (/ (+ (/ x (square y))
+            (* 2 y))
+         3.0))
+    (cube-root-iter 1))
 
   (println (cube-root 3))
-  (println (cube (cube-root 3))))
+  (println (cube (cube-root 3)))
+
+  (println (cube-root -10))
+  (println (cube (cube-root -10))))
+
+;;; Exercise 1.10
+(define (ex1.10)
+  (define (A x y)
+    (cond ((= y 0) 0)
+          ((= x 0) (* 2 y))
+          ((= y 1) 2)
+          (else (A (- x 1) (A x (- y 1))))))
+  (println (A 1 10))
+  (println (A 2 4))
+  (println (A 3 3)))
+(ex1.10)
