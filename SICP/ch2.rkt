52ce7472ee428da6255260afc9c65aada8419ea2
orange <alisper@outlook.com>
2022-07-06 23:48:30 +0800

    ex2.7 实现区间运算

--- SICP/ch2.rkt
+++ SICP/ch2.rkt
@@ -154,4 +154,33 @@
   (println zero)
   )
 (link 'ex2.6 ex2.6)
+
+((lambda ()
+   (define (add-interval x y)
+     (make-interval (+ (lower-bound x) (lower-bound y))
+                    (+ (upper-bound x) (upper-bound y))))
+   (define (mul-interval x y)
+     (let ((p1 (* (lower-bound x) (lower-bound y)))
+           (p2 (* (lower-bound x) (upper-bound y)))
+           (p3 (* (upper-bound x) (lower-bound y)))
+           (p4 (* (upper-bound x) (upper-bound y))))
+       (make-interval (min p1 p2 p3 p4)
+                      (max p1 p2 p3 p4))))
+   (define (div-interval x y)
+     (mul-interval x
+                   (make-interval (/ 1.0 (upper-bound y))
+                                  (/ 1.0 (lower-bound y)))))
+
+   ;;; ex2.7
+   (define (make-interval a b) (cons a b))
+   (define (lower-bound p) (car p))
+   (define (upper-bound p) (cdr p))
+   
+   (define (ex2.7)
+     (define p1 (make-interval 1 2))
+     (define p2 (make-interval 3 4))
+     (println (add-interval p1 p2)))
+   (link 'ex2.7 ex2.7)
+   ))
+
 (last-exercise)
