fbfa50e52ee8041f55778b87a083588281f4c2bb
orange <alisper@outlook.com>
2022-07-10 21:01:53 -0700

    child walk

--- Racket/gui/animate01.rkt
+++ Racket/gui/animate01.rkt
@@ -5,11 +5,14 @@
 
 (define (child-scene elapse)
   (underlay/xy (rectangle 1000 1000 "solid" "black")
-               (+ 100 (* 100 (sin (/ elapse 30.0))))
-               (+ 100 (* 100 (sin (/ elapse 30.0))))
-               (child elapse)))
+               500
+               500
+               ;(+ 100 (* 100 (sin (/ elapse 30.0))))
+               ;(+ 100 (* 100 (sin (/ elapse 30.0))))
+               (scale 2 (child elapse))))
 
-(define x (bitmap "child.png"))
+(define x (scale 1 (bitmap "child.png")))
+
 (define gv4 (vector
              (place-image x -32 -48 (empty-scene 32 48 (make-color 0 0 0 0)))
              (place-image x 0 -48 (empty-scene 32 48 (make-color 0 0 0 0)))
@@ -20,7 +23,7 @@
   (lambda (elapse)
     (underlay/align "center"
                     "center"
-                    (vector-ref gv4 (remainder (floor (/ elapse 10)) (vector-length gv4)))
+                    (vector-ref gv4 (remainder (floor (/ elapse 5)) (vector-length gv4)))
                     (empty-scene 0 0))))
 
 (animate child-scene)
