9befed4fec94c49d950d533946bc7fe35c9d213f
orange <alisper@outlook.com>
2022-07-16 22:35:35 +0800

    旋转图像

--- Racket/draw-list.rkt
+++ Racket/draw-list.rkt
@@ -4,17 +4,25 @@
 
 (define c1 (rectangle 30 30 'outline (make-color 255 0 0)))
 (define c2 (rectangle 30 30 'outline (make-color 0 0 0)))
-(define dx 40)
-(define dy 40)
+;(define dx 40)
+;(define dy 40)
+;
+(define s (empty-scene 500 500))
+;(define (f x y items s c)
+;  (cond [(null? items) s]
+;        [(pair? (car items))
+;         (begin
+;           (f x (+ dy y) (cdr items) (f x (+ dy y) (car items) s c) c))]
+;        [else
+;         (begin
+;           (f (+ dx x) y (cdr items) (place-image c x y s) c))]))
+;(define lst (list (list 1 2) 3 4))
+;(f 100 100 lst s c1)
 
-(define s (empty-scene 1000 1000))
-(define (f x y items s c)
-  (cond [(null? items) s]
-        [(pair? (car items))
-         (begin
-           (f x (+ dy y) (cdr items) (f x (+ dy y) (car items) s c) c))]
-        [else
-         (begin
-           (f (+ dx x) y (cdr items) (place-image c x y s) c))]))
-(define lst (list (cons 1 2) 3 4))
-(f 100 100 lst s c1)
+(define arrow-r (overlay/xy
+ (text "   >" 12 (make-color 255 0 0))
+ 0
+ 10
+ (line 20 0 (make-color 255 0 0))))
+
+(place-image arrow-r 100 100 s)
